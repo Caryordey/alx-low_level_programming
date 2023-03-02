af9c3ef7f1c9f37b729e788f63bb97e6b1926cc3
Kayode Olubobokun <kaycaryordey@gmail.com>
2023-03-01 18:24:09 -0800

    7 Leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ * @n: the string required
+ *
+ * Return: the encoded string
+ */
+char *leet(char *n)
+{
+	int i, j;
+
+	int tokens[] = {65, 69, 79, 84, 76};
+	int repl[] = {4, 3, 0, 7, 1};
+	int tokens_len = 5;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < tokens_len; j++)
+		{
+			if (n[i] == tokens[j] || n[i] == tokens[j] + 32)
+				n[i] = repl[j] + 48;
+		}
+	}
+
