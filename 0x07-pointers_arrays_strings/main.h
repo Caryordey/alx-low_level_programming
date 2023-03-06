8539a2658e16739973bd295da321aa5d0a2bac05
Kayode Olubobokun <kaycaryordey@gmail.com>
2023-03-06 15:41:49 -0800

    0 Memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif
