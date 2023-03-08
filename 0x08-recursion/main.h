036e25ae2da8d04fc26a497fc6da512ca35eedd8
Kayode Olubobokun <kaycaryordey@gmail.com>
2023-03-07 16:32:12 -0800

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void _puts_recursion(char *s);
+int is_prime(int, int);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int _palindrome(char *, int);
+void _print_rev_recursion(char *s);
+int wildcmp(char *, char *);
+int _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recur(int, int);
+int _sqrt_recursion(int n);
+
+#endif
